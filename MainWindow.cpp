bea4874affea27fd5e6daa60ef1c7ea979fdc4f3
letsfindaway <me@letsfindaway.de>
2022-11-22 09:26:14 +0100

    show web page using QML
    - same error, CSS cursors not shown

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,29 +1,28 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
-#include <QDebug>
-#include <QTimer>
 #include <QGraphicsProxyWidget>
-#include <QWebEnginePage>
-#include <QWebEngineView>
-#include <QWebEngineScript>
-#include <QWebEngineScriptCollection>
+#include <QQuickWidget>
+#include <QtWebEngine>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QtWebEngine::initialize();
 
     QGraphicsScene* scene = new QGraphicsScene();
     QGraphicsProxyWidget* proxy = new QGraphicsProxyWidget();
-    QWebEngineView* view = new QWebEngineView();
-    proxy->setWidget(view);
+
+    const QUrl url(QStringLiteral("qrc:/webview.qml"));
+    QQuickWidget* qmlWidget = new QQuickWidget(url);
+
+    proxy->setWidget(qmlWidget);
     scene->addItem(proxy);
     proxy->setPos(-400, -300);
     proxy->resize(800, 600);
     ui->graphicsView->setScene(scene);
-    view->load(QUrl("https://www.w3schools.com/cssref/tryit.php?filename=trycss_cursor"));
 }
 
 MainWindow::~MainWindow()
---
+++ webview.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.15
+import QtWebEngine 1.10
+
+WebEngineView  {
+    width: 800
+    height: 600
+    visible: true
+    url: "https://www.w3schools.com/cssref/tryit.php?filename=trycss_cursor"
+}
